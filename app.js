0d06b542fa35ca0c520b77d3e4c96e954ab6df74
Mukut Chakrabarty <mukutckb1@gmail.com>
2022-08-22 21:53:06 +0600

    validation completed

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ const selectedPlayersList = document.getElementById('selected-player-list')
 const totalPlayerExpenses = document.getElementById('calculate')
 const calculateTotalCost = document.getElementById('calculate-total')
 
-// capturing all button 
+// capturing all select player button 
 for(const selectBtn of selectButtons) {
     selectBtn.addEventListener('click', selectButton)
 } 
 
 // select button function 
 function selectButton(btn){
-    const disableBtn = btn.target
     const playerName = btn.target.parentNode.parentNode.children[0].innerText
     const createLi = document.createElement('li')
     createLi.classList.add('text-white','font-medium','text-lg','leading-10')
@@ -21,38 +20,59 @@ function selectButton(btn){
 
      }else{
        selectedPlayersList.appendChild(createLi)
-       disableBtn.disabled = true
+       btn.target.disabled = true
 }
 }
 
 // converting string to number
+
 function strToNum(id){
     const elementId = document.getElementById(id)
     const elementIdToNum = parseInt(elementId.value)
     return elementIdToNum;
 }
 // set innerText function 
+
 function setInnerText(id , value){
    document.getElementById(id).innerText = value
 }
+
+
 // total player expense 
 
 function totalPlayerExpense(){
   const perPlayerCost = strToNum('per-player')
   const totalPlayers = selectedPlayersList.children.length
   const totalExpenses = perPlayerCost * totalPlayers
-  setInnerText('total-player-cost', totalExpenses)  
+
+
+  if(totalPlayers > 0){    //   checking players length 
+   
+    if(perPlayerCost > 0){     //checking total players cost
+        
+        setInnerText('total-player-cost', totalExpenses)
+    }else{
+        return alert('Please enter a valid amount')
+    }
+  }else{
+    return alert('Select Players First')
+  }
   return totalExpenses;
 }
 
 // calculate total costing
 
 function calculateTotalCosting(){
-    console.log(totalPlayerExpense());
     const managerCost = strToNum('manager')
     const coachCost= strToNum('coach')
-    const totalCost = totalPlayerExpense() + managerCost + coachCost
-    setInnerText('total-cost', totalCost)
+   // checking managerCost & coachCost is greater than 0 or not   
+    if((managerCost > 0) && (coachCost > 0)){
+       const totalCost = totalPlayerExpense() + managerCost + coachCost
+       console.log(totalCost);
+       setInnerText('total-cost', totalCost)
+}else{
+    return alert('Please enter a valid amount')
+}
 }
 
 // event listeners
